7bc48e71774b420028cfd2e04948a7884e08f656
Talyslan Canabarro <78499700+Talyslan@users.noreply.github.com>
2024-06-10 11:21:41 -0300

    add: card component

---
+++ src/components/TheCard.vue
@@ -0,0 +1,126 @@
+<!-- The Card HTML -->
+<template>
+    <div class="card">
+        <div 
+            class="card__image" 
+            :style="{ backgroundImage: `url(${ img })` }">
+        </div>          
+
+        <div class="card__text">
+            <div class="up">
+                <span> {{ name }} </span>
+                <span>{{ status }} - {{ species }}</span>
+            </div>
+
+            <a href="">See More!</a>
+        </div>
+    </div>
+</template>
+
+<!-- The Card Script -->
+<script>
+
+export default {
+    name: "TheCard",
+    props: {
+        img: {
+            type: String,
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        species: {
+            type: String,
+            required: true
+        }
+    }
+}
+
+</script>
+
+<!-- The Card Style -->
+<style>
+.card,
+a {
+    color: var(--clr-white);
+}
+
+.card {
+    position: relative;
+    height: 380px;
+    width: 300px;
+}
+
+/* Image */
+.card__image {
+    width: 100%;
+    height: 100%;
+    background-size: cover;
+    background-position: center;
+    position: relative;
+    border-radius: 4px;
+
+    box-shadow: 0 0 .3em var(--clr-gray);
+}
+
+.card__image::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background: linear-gradient(to top, rgba(0, 0, 0, .5) 10%, rgba(0, 0, 0, 0) 50%);
+}
+
+/* Text */
+.card__text {
+    text-align: center;
+    position: absolute;
+    bottom: 1em;
+    left: 50%;
+    transform: translateX(-50%);
+
+    z-index: 2;
+    width: 90%;
+    border-right: 3px solid var(--clr-green);
+    border-left: 3px solid var(--clr-green);
+}
+
+.card__text .up {
+    display: flex;
+    flex-direction: column;
+}
+
+.card__text .up span:nth-child(1) {
+    font-family: "Lalezar", "Josefin Sans", sans-serif;
+    font-size: 2rem;
+}
+
+.card__text .up span:nth-child(2) {
+    font-size: 1.2rem;
+    font-weight: 600;
+    transform: translateY(-.5em);
+}
+
+.card__text a {
+    font-size: .9rem;
+}
+
+.card__text a::after {
+    content: '';
+    display: inline-block;
+    width: 1em;
+    height: 1em;
+    margin-left: 8px;
+    background: url('../../public/arrows/arrow-right.svg') no-repeat center center;
+    background-size: contain;
+    position: absolute;
+}
+</style>
--- src/components/main/TheCharacters.vue
+++ src/components/main/TheCharacters.vue
@@ -10,21 +10,12 @@
         /> 
 
         <div class="box">
-            <div class="card" v-for="obj in listCharacters" :key="obj.id">
-                <div 
-                    class="card__image" 
-                    :style="{ backgroundImage: `url(${obj.image})` }">
-                </div>          
-
-                <div class="card__text">
-                    <div class="up">
-                        <span> {{ obj.name }} </span>
-                        <span>{{ obj.status }} - {{ obj.species }}</span>
-                    </div>
-
-                    <a href="">See More!</a>
-                </div>
-            </div>
+            <TheCard v-for="obj in listCharacters" :key="obj.id"
+                :img="obj.image"
+                :name="obj.name"
+                :status="obj.status"
+                :species="obj.species"
+            />
         </div>
         
         <TheButton text="See more!" bg="bg-green" txt="txt-black" />
@@ -33,17 +24,20 @@
 
 <!-- The Caracters Script -->
 <script>
-import TheTitle from '../TheTitle.vue';
 import characterIcon from '@/assets/icon/icon-characters.svg';
+// Components
+import TheTitle from '../TheTitle.vue';
 import TheButton from '../TheButton.vue';
+import TheCard from '../TheCard.vue';
 
-const apiuUrl = 'https://rickandmortyapi.com/api/'
+const apiUrl = 'https://rickandmortyapi.com/api/'
 const endpointCharacter = 'character'
 
 export default {
     name: "TheCharacters",
     components: {
         TheTitle,
+        TheCard,
         TheButton
     },
     data() {
@@ -54,7 +48,7 @@ export default {
     },
     async created() {
         try {
-            const response = await fetch(apiuUrl + endpointCharacter)
+            const response = await fetch(apiUrl + endpointCharacter);
             const data = await response.json();
             this.listCharacters = this.action(data.results, 6);
             
@@ -87,90 +81,26 @@ export default {
     gap: 3em;
 }
 
-.card,
-a {
-    color: var(--clr-white);
-}
-
 /* Box and Card */
 .box {
     display: grid;
     grid-template-columns: repeat(3, 1fr);
     grid-template-rows: repeat(2, 1fr);
+    justify-items: center;
+    align-items: center;
     gap: 3em;
     width: 100%;
 }
 
-.card {
-    position: relative;
-    height: 380px;
-    width: 300px;
+@media (max-width: 1200px) {
+    .box {
+        grid-template-columns: repeat(2, 1fr);
+    }
 }
 
-/* Image */
-.card__image {
-    width: 100%;
-    height: 100%;
-    background-size: cover;
-    background-position: center;
-    position: relative;
-    border-radius: 4px;
-
-    box-shadow: 0 0 .3em var(--clr-gray);
-}
-
-.card__image::before {
-    content: '';
-    position: absolute;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    background: linear-gradient(to top, rgba(0, 0, 0, .5) 10%, rgba(0, 0, 0, 0) 50%);
-}
-
-/* Text */
-.card__text {
-    text-align: center;
-    position: absolute;
-    bottom: 1em;
-    left: 50%;
-    transform: translateX(-50%);
-
-    z-index: 2;
-    width: 90%;
-    border-right: 3px solid var(--clr-green);
-    border-left: 3px solid var(--clr-green);
-}
-
-.card__text .up {
-    display: flex;
-    flex-direction: column;
-}
-
-.card__text .up span:nth-child(1) {
-    font-family: "Lalezar", "Josefin Sans", sans-serif;
-    font-size: 2rem;
-}
-
-.card__text .up span:nth-child(2) {
-    font-size: 1.2rem;
-    font-weight: 600;
-    transform: translateY(-.5em);
-}
-
-.card__text a {
-    font-size: .9rem;
-}
-
-.card__text a::after {
-    content: '';
-    display: inline-block;
-    width: 1em;
-    height: 1em;
-    margin-left: 8px;
-    background: url('../../../public/arrows/arrow-right.svg') no-repeat center center;
-    background-size: contain;
-    position: absolute;
+@media (max-width: 810px) {
+    .box {
+        grid-template-columns: repeat(1, 1fr);
+    }
 }
 </style>
